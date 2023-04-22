68887d8d6ca5bfdc3ca9e9665be6ec82cda67d22
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-21 23:18:54 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Faça um programa que calcule quanto dinheiro é necessário possuir em um investimento para conseguir sua independência financeira.
+
+Console.WriteLine("Programa de investimeto financeira\n");
+
+Console.Write("informe seus gastos mensais: R$");
+double gastosMensais = double.Parse(Console.ReadLine()!);
+
+Console.Write("Informe os Rendimentos mensais (%): ");
+double dividendYield = double.Parse(Console.ReadLine()!);
+
+double valorNecessario = gastosMensais / dividendYield / 100;
+
+Console.WriteLine($"Para receber R$ {gastosMensais} todo mês com {dividendYield}% de dividend yield, é necessário investir R$ {valorNecessario.ToString("N2")}");
+
